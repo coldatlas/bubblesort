c6425cd7103500656d7cd5e6d1e0b3261d384c1e
Mina <cccoldatlas@gmail.com>
2020-09-17 15:05:22 +0200

    added rb file

---
+++ bubblesort.rb
@@ -0,0 +1,16 @@
+def bubblesort(arr)
+
+  (arr.length-1).times do |i|
+
+   loop do
+     
+    switch = true
+   if arr[i] > arr[i+1]
+     arr [i], arr[i+1] = arr[i+1], arr[i]
+     switch = false
+  end
+
+ if switch = true
+ 
+    
+end
