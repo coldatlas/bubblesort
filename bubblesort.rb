94b213db665d3e842f7d92af86bc195d6a77d65f
Lidya1234 <lidya94gher@gmail.com>
2020-09-17 19:52:01 +0300

    Fixed linter errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,47 +1,39 @@
 def bubblesort(arr)
-  
+  loop do
+    swapped = false
 
-    loop do
-      
-      swapped = false
-  
-      (arr.length-1).times do |i|
-        if arr[i] > arr[i+1]
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-          swapped = true
-        end
+    (arr.length - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
       end
-  
-      break if not swapped
     end
-  
-     arr
+
+    break unless swapped
   end
-  
-  
-  print bubblesort([11,5,7,6,15])
-  
-  puts
-  
-  def bubble_sort_by(arr)
-    
-  
-    
-      loop do
-        swapped = false
-    
-    
-        (arr.length - 1).times do |i|
-          if yield(arr[i], arr[i + 1]) > 0
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-            swapped = true
-          end
-        end
-        break if not swapped
+
+  arr
+end
+
+print bubblesort([11, 5, 7, 6, 15])
+
+puts
+
+def bubble_sort_by(arr)
+  loop do
+    swapped = false
+
+    (arr.length - 1).times do |i|
+      if yield(arr[i], arr[i + 1]) > 0
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
       end
-      print arr
     end
-    
-    bubble_sort_by(["hi","hello", "hey"]) do |left, right|
-      left.length - right.length
-    end
+    break unless swapped
+  end
+  print arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
